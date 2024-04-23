6654e43f163fa41ffbf08694898e16662a6425b3
Adel Bako <adelabako96@gmail.com>
2024-04-23 06:48:16 -0400

    website project

---
+++ scripts/myScript.js
@@ -0,0 +1,10 @@
+
+/******ALERT BOX ADDED FOR INDEX PAGE ONLY******/
+function myFunction() {
+    alert("This website is strictly a school project and has NO affiliation with any local business.Thank you!");
+  }
+
+
+
+
+
